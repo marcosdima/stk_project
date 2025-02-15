0281e1063849e770a1aa77fdcc987bf47797482f
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-15 18:35:37 -0300

    Create Sticker test

--- stk_backend/tests/stickers.rs
+++ stk_backend/tests/stickers.rs
@@ -127,4 +127,33 @@ mod tests {
         assert_eq!(body, "Sticker not found");
 
     }
+
+    #[actix_web::test]
+    async fn test_create_sticker() {
+        let pool = web::Data::new(common::init_test_db_pool());
+
+        let app = test::init_service(
+            App::new()
+                .app_data(pool.clone())
+                .configure(stk_backend::routes::stickers::configure)
+        ).await;
+
+        let new_sticker_data = get_stk_default_data(1);
+
+        // Creates a sticker.
+        let req = test::TestRequest::default()
+            .method(Method::POST)
+            .uri(&format!("/stickers"))
+            .insert_header(ContentType::json())
+            .set_payload(serde_json::to_string(&new_sticker_data).unwrap())
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        // Gets the new sticker.
+        let body = test::read_body(resp).await;
+        let new_sticker: Sticker = serde_json::from_slice(&body).unwrap();
+
+        expect_n_stk(&app, vec![new_sticker]).await;
+    }
 }
