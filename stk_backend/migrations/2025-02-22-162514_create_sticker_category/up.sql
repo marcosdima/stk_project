b64401a3b3274bde6829ac562d451cee80e15b6c
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-23 16:07:57 -0300

    Fix foreign key sintax

--- stk_backend/migrations/2025-02-22-162514_create_sticker_category/up.sql
+++ stk_backend/migrations/2025-02-22-162514_create_sticker_category/up.sql
@@ -3,5 +3,5 @@ CREATE TABLE sticker_category (
     category_id TEXT NOT NULL,
     PRIMARY KEY (sticker_id, category_id),
     FOREIGN KEY(category_id) REFERENCES category(id),
-    FOREIGN KEY(category_id) REFERENCES category(id)
+    FOREIGN KEY(sticker_id) REFERENCES sticker(id)
 );
