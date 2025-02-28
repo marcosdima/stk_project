667930cea3069e8c730e021abe7a5dad9ac5b2ad
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-28 18:22:59 -0300

    Setup file

--- stk_backend/tests/common/mod.rs
+++ stk_backend/tests/common/mod.rs
@@ -1,49 +1,26 @@
+mod setup;
 pub mod default;
 
+pub use setup::get_app;
+
 use actix_http::Request;
+
 use actix_web::{
     dev::{
         Service,
         ServiceResponse
-    }, http::header::ContentType, test, web::{self, Data}, App, Error
+    },
+    http::header::ContentType,
+    test,
+    Error
 };
 
-use diesel::{
-    r2d2::{
-        self,
-        ConnectionManager,
-    },
-    SqliteConnection,
-};
-use diesel_migrations::{
-    self,
-    embed_migrations,
-    EmbeddedMigrations,
-    MigrationHarness,
-};
 use serde::Deserialize;
+
 use stk_backend::{
     models::BasicModel,
     routes::DbPool,
 };
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
-
-pub fn init_test_db_pool() -> DbPool {
-    let conn_spec = ":memory:";
-    let manager = ConnectionManager::<SqliteConnection>::new(conn_spec);
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create DB pool");
-
-    let conn = &mut pool.get().unwrap();
-    run_migrations(conn);
-
-    pool
-}
-
-pub fn run_migrations(conn: &mut SqliteConnection) {
-    conn.run_pending_migrations(MIGRATIONS).unwrap();
-}
 
 pub fn create_test_objects<T: BasicModel>(
     pool: &DbPool,
@@ -103,17 +80,3 @@ pub async fn expect_n_elements<T: BasicModel>(
     assert_eq!(expected, categories);
 }
 
-pub async fn get_app() -> (impl Service<Request, Response = ServiceResponse, Error = Error>, Data<DbPool>) {
-    let pool = web::Data::new(init_test_db_pool());
-
-    let app = test::init_service(
-        App::new()
-            .app_data(pool.clone())
-            .configure(stk_backend::routes::stickers::configure)
-            .configure(stk_backend::routes::categories::configure)
-            .configure(stk_backend::routes::tags::configure)
-            .configure(stk_backend::routes::artists::configure)
-    ).await;
-
-    (app, pool)
-}
---
+++ stk_backend/tests/common/setup.rs
@@ -0,0 +1,66 @@
+use actix_web::{
+    dev::{
+        Service,
+        ServiceResponse
+    },
+    test,
+    web::{
+        self,
+        Data
+    },
+    App,
+    Error
+};
+
+use diesel::{
+    r2d2::{
+        self,
+        ConnectionManager,
+    },
+    SqliteConnection,
+};
+
+use diesel_migrations::{
+    self,
+    embed_migrations,
+    EmbeddedMigrations,
+    MigrationHarness,
+};
+
+use stk_backend::routes::DbPool;
+
+use actix_http::Request;
+
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
+pub fn init_test_db_pool() -> DbPool {
+    let conn_spec = ":memory:";
+    let manager = ConnectionManager::<SqliteConnection>::new(conn_spec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create DB pool");
+
+    let conn = &mut pool.get().unwrap();
+    run_migrations(conn);
+
+    pool
+}
+
+pub fn run_migrations(conn: &mut SqliteConnection) {
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
+}
+
+pub async fn get_app() -> (impl Service<Request, Response = ServiceResponse, Error = Error>, Data<DbPool>) {
+    let pool = web::Data::new(init_test_db_pool());
+
+    let app = test::init_service(
+        App::new()
+            .app_data(pool.clone())
+            .configure(stk_backend::routes::stickers::configure)
+            .configure(stk_backend::routes::categories::configure)
+            .configure(stk_backend::routes::tags::configure)
+            .configure(stk_backend::routes::artists::configure)
+    ).await;
+
+    (app, pool)
+}
