48ddc24b0a60c23e9c3d59cdb0b621b500e40ee6
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-28 16:08:51 -0300

    Add ON UPDATE and ON DELETE to fks

--- stk_backend/migrations/2025-02-22-162514_create_sticker_category/up.sql
+++ stk_backend/migrations/2025-02-22-162514_create_sticker_category/up.sql
@@ -2,6 +2,10 @@ CREATE TABLE sticker_category (
     sticker_id TEXT NOT NULL,
     category_id TEXT NOT NULL,
     PRIMARY KEY (sticker_id, category_id),
-    FOREIGN KEY(category_id) REFERENCES category(id),
+    FOREIGN KEY(category_id) REFERENCES category(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     FOREIGN KEY(sticker_id) REFERENCES sticker(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
--- stk_backend/migrations/2025-02-25-133443_create_sticker_tag/up.sql
+++ stk_backend/migrations/2025-02-25-133443_create_sticker_tag/up.sql
@@ -2,6 +2,10 @@ CREATE TABLE sticker_tag (
     tag_name TEXT NOT NULL,
     sticker_id TEXT NOT NULL,
     PRIMARY KEY(tag_name, sticker_id),
-    FOREIGN KEY(tag_name) REFERENCES tag(name),
+    FOREIGN KEY(tag_name) REFERENCES tag(name)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     FOREIGN KEY(sticker_id) REFERENCES sticker(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
