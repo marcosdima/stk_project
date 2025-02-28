5131eab28053f42c774c6aafbf9151493bf8b826
marcosdima <marcosdimatteo.dev@gmail.com>
2025-02-28 18:22:17 -0300

    Missing get_app implementation

--- stk_backend/tests/stickers/get.rs
+++ stk_backend/tests/stickers/get.rs
@@ -6,11 +6,7 @@ fn create_stickers(pool: &DbPool, n: u16) -> Vec<Sticker> {
 
 #[actix_web::test]
 async fn test_get_stickers_empty() {
-    let app = test::init_service(
-        App::new()
-            .app_data(web::Data::new(common::init_test_db_pool()))
-            .configure(stk_backend::routes::stickers::configure)
-    ).await;
+    let (app, _) = get_app().await;
 
     let empty: Vec<Sticker> = vec![];
     common::expect_n_elements(&app, "/stickers", empty).await;
