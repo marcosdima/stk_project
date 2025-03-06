73ccd17c2237c087a189b21f0a9a297544767d77
marcosdima <marcosdimatteo.dev@gmail.com>
2025-03-05 21:22:56 -0300

    Modification: An sticker can have multiple artists

--- stk_backend/migrations/2025-02-28-185444_create_artist_sticker/up.sql
+++ stk_backend/migrations/2025-02-28-185444_create_artist_sticker/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE artist_sticker (
     artist_id TEXT NOT NULL,
-    sticker_id TEXT NOT NULL UNIQUE,
+    sticker_id TEXT NOT NULL,
     PRIMARY KEY(artist_id, sticker_id),
     FOREIGN KEY(artist_id) REFERENCES artist(id)
         ON UPDATE CASCADE
