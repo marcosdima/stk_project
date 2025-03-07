ee4793c745c8840c18bd80f31a3a205f69e49da5
marcosdima <marcosdimatteo.dev@gmail.com>
2025-03-07 16:01:05 -0300

    Empty tag name test

--- stk_backend/tests/tags/create.rs
+++ stk_backend/tests/tags/create.rs
@@ -22,3 +22,20 @@ async fn test_create_tag() {
 
     assert_eq!(Tag::get_all(&pool).unwrap(), vec![new_tag])
 }
+
+#[actix_web::test]
+async fn test_create_tag_empty_name() {
+    let (app, _) = get_app().await;
+
+    let new_tag_data = NewTag::new("".to_owned());
+
+    // Creates a tag.
+    let req = test::TestRequest::default()
+        .method(Method::POST)
+        .uri(&format!("/tags"))
+        .insert_header(ContentType::json())
+        .set_payload(serde_json::to_string(&new_tag_data).unwrap())
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_client_error());
+}
