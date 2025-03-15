3786eaf3392d51fd931c95b662024fc0c4699c08
marcosdima <marcosdimatteo.dev@gmail.com>
2025-03-15 15:41:59 -0300

    New imports

--- stk_backend/tests/mod.rs
+++ stk_backend/tests/mod.rs
@@ -20,9 +20,13 @@ mod utils;
 
 pub use actix_web::{
     http::{
-        header::ContentType,
+        header::{
+            ContentType,
+            AUTHORIZATION,
+        },
         Method
     },
+    web::Bytes,
     test,
     web,
     App,
@@ -39,22 +43,25 @@ pub use stk_backend::{
         artist_sticker::*,
         users::*,
         BasicModel,
-        Model
+        Model,
     },
-    routes::DbPool
+    routes::DbPool,
+    utils::generate_token,
+    errors::AppError,
 };
 
 pub use crate::common::{
     default::{
         self,
-        get_category_default,
-        get_sticker_default,
-        get_tag_default,
-        get_artist_default,
-        get_user_default,
+        *
     },
+    admin::*,
+    requests::*,
     get_element,
-    get_app
+    get_app,
+    get_just_app,
+    get_just_pool,
+    expect_error,
 };
 
 pub use uuid::Uuid;
