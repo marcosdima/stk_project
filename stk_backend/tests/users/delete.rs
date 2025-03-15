8363435df47d431dcb0f3a42b310ebf84ea22969
marcosdima <marcosdimatteo.dev@gmail.com>
2025-03-15 15:46:57 -0300

    Middleware  test on user delete - First implementation of expect_error

--- stk_backend/tests/users/delete.rs
+++ stk_backend/tests/users/delete.rs
@@ -5,41 +5,98 @@ fn create_users(pool: &DbPool, n: u16) -> Vec<User> {
 }
 
 #[actix_web::test]
-async fn test_delete_users() {
+async fn test_delete_user() {
     let (app, pool) = get_app().await;
 
     // Gets id from a new user.
     let created = create_users(&pool, 1).pop().unwrap().id;
 
     // Should return a succes message.
-    let req = test::TestRequest::default()
-        .method(Method::DELETE)
-        .uri(&format!("/users/{created}"))
-        .insert_header(ContentType::plaintext())
-        .to_request();
-    let resp = test::call_service(&app, req).await;
+    let headers = vec![
+        get_admin_token_header(&pool),
+        get_plain_text_header(),
+    ];
+    
+    let resp = basic_request(
+        &app,
+        &format!("/users/{created}"),
+        Method::DELETE,
+        headers,
+        "",
+    ).await;
+
     assert!(resp.status().is_success());
     let body = test::read_body(resp).await;
     assert_eq!(body, "User deleted successfully");
 
-    // Gets users, it should be an empty vector.
-    let empty: Vec<User> = vec![];
-    common::expect_n_elements::<User>(&app, "/users", empty).await;
+    // Gets user, it shouldn't exists.
+    assert!(User::get_by_id(&pool, created).is_err());
 }
 
 #[actix_web::test]
-async fn test_delete_users_not_found() {
-    let (app, _) = get_app().await;
+async fn test_delete_user_not_found() {
+    let (app, pool) = get_app().await;
 
     // Should return a succes message.
-    let req = test::TestRequest::default()
-        .method(Method::DELETE)
-        .uri(&format!("/users/id-not-found"))
-        .insert_header(ContentType::plaintext())
-        .to_request();
-    let resp = test::call_service(&app, req).await;
-    assert!(resp.status().is_client_error());
-    let body = test::read_body(resp).await;
-    assert_eq!(body, "User not found");
+    let headers = vec![
+        get_admin_token_header(&pool),
+        get_plain_text_header(),
+    ];
+    
+    let resp = basic_request(
+        &app,
+        "/users/id-not-found",
+        Method::DELETE,
+        headers,
+        "",
+    ).await;
 
+    expect_error(AppError::NotFound("User not found"), resp).await;
+}
+
+#[actix_web::test]
+async fn test_delete_user_unauthorized() {
+    let (app, pool) = get_app().await;
+
+    // Gets id from a new user.
+    let created = create_users(&pool, 1).pop().unwrap().id;
+
+    // Should return a succes message.
+    let headers = vec![
+        get_plain_text_header(),
+    ];
+    
+    let resp = basic_request(
+        &app,
+        &format!("/users/{created}"),
+        Method::DELETE,
+        headers,
+        "",
+    ).await;
+
+    expect_error(AppError::InvalidToken, resp).await;
+}
+
+#[actix_web::test]
+async fn test_delete_user_forbbiden() {
+    let (app, pool) = get_app().await;
+
+    // Gets id from a new user.
+    let created = create_users(&pool, 1).pop().unwrap().id;
+
+    // Should return a succes message.
+    let headers = vec![
+        get_token_header(generate_token(&created).unwrap()),
+        get_plain_text_header(),
+    ];
+
+    let resp = basic_request(
+        &app,
+        &format!("/users/{created}"),
+        Method::DELETE,
+        headers,
+        "",
+    ).await;
+
+    expect_error(AppError::RoleNeeded, resp).await;
 }
