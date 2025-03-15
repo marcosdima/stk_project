b2cc15712be9edb60f963e4802fff1e7d5141065
marcosdima <marcosdimatteo.dev@gmail.com>
2025-03-15 00:19:09 -0300

    First middleware attempt

--- stk_backend/src/routes/hello.rs
+++ stk_backend/src/routes/hello.rs
@@ -1,10 +1,37 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{
+    get,
+    web,
+    HttpResponse,
+    Responder,
+    dev::{
+        ServiceRequest,
+        ServiceResponse,
+    },
+    body::MessageBody,
+    Error,
+    middleware::{
+        from_fn,
+        Next,
+    },
+};
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+async fn print(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
+    println!("Hello route: {}", req.path());
+    next.call(req).await
+}
+
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(hello);
+    let scope = web::scope("")
+        .wrap(from_fn(print))
+        .service(hello);
+
+    cfg.service(scope);
 }
